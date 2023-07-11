db274c644f643f830c35b6a92edd328bf7e24f59
Klaus Wakonig <klaus.wakonig@psi.ch>
2023-07-11 17:58:07 +0200

    feat: added config plotter

--- setup.py
+++ setup.py
@@ -6,6 +6,7 @@ if __name__ == "__main__":
     setup(
         install_requires=[
             "pyqt5",
+            "pyqtgraph",
         ],
         extras_require={"dev": ["pytest", "pytest-random-order", "coverage", "pytest-qt"]},
         version=__version__,
