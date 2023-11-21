c1743267625c7b368e871e0e2ce0761444d1075e
wyzula-jan <133381102+wyzula-jan@users.noreply.github.com>
2023-11-22 00:06:16 +0100

    build: disabled support to PySide2/PySide6, due to no QScintilla support; added pyqtdarktheme

--- setup.py
+++ setup.py
@@ -4,32 +4,32 @@ __version__ = "0.31.0"
 
 # Default to PyQt6 if no other Qt binding is installed
 qt_dependency = "PyQt6>=6.0"
+qscintilla_dependency = "PyQt6-QScintilla"
 try:
     import PyQt5
 except ImportError:
-    try:
-        import PySide2
-    except ImportError:
-        try:
-            import PySide6
-        except ImportError:
-            pass
-        else:
-            qt_dependency = "PySide6>=6.0"
-    else:
-        qt_dependency = "PySide2>=5.9"
+    pass
 else:
     qt_dependency = "PyQt5>=5.9"
+    qscintilla_dependency = "QScintilla"
 
 if __name__ == "__main__":
     setup(
-        install_requires=["pydantic", qt_dependency, "qtpy", "pyqtgraph", "bec_lib", "zmq", "h5py"],
+        install_requires=[
+            "pydantic",
+            qt_dependency,
+            qscintilla_dependency,
+            "qtpy",
+            "pyqtgraph",
+            "bec_lib",
+            "zmq",
+            "h5py",
+            "pyqtdarktheme",
+        ],
         extras_require={
             "dev": ["pytest", "pytest-random-order", "coverage", "pytest-qt", "black"],
             "pyqt5": ["PyQt5>=5.9"],
             "pyqt6": ["PyQt6>=6.0"],
-            "pyside2": ["PySide2>=5.9"],
-            "pyside6": ["PySide6>=6.0"],
         },
         version=__version__,
     )
