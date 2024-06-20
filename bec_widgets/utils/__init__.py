cbbd23aa33095141e4c265719d176c4aa8c25996
wakonig_k <klaus.wakonig@psi.ch>
2024-06-20 13:50:44 +0200

    fix(pyqt): webengine must be imported before qcoreapplication

--- bec_widgets/utils/__init__.py
+++ bec_widgets/utils/__init__.py
@@ -1,3 +1,5 @@
+from qtpy.QtWebEngineWidgets import QWebEngineView
+
 from .bec_connector import BECConnector, ConnectionConfig
 from .bec_dispatcher import BECDispatcher
 from .bec_table import BECTable
