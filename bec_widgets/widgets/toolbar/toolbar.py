96863adf53c15112645d20eb6200733617801c6d
wyzula-jan <jan.wyzula@psi.ch>
2024-07-02 15:33:46 +0200

    refactor(toolbar): cleanup and adjusted colors

--- bec_widgets/widgets/toolbar/toolbar.py
+++ bec_widgets/widgets/toolbar/toolbar.py
@@ -1,123 +1,70 @@
 from abc import ABC, abstractmethod
+from collections import defaultdict
 
 # pylint: disable=no-name-in-module
-from qtpy.QtCore import QSize, QTimer
+from qtpy.QtCore import QSize
 from qtpy.QtGui import QAction
-from qtpy.QtWidgets import QApplication, QStyle, QToolBar, QWidget
+from qtpy.QtWidgets import QHBoxLayout, QLabel, QSpinBox, QToolBar, QWidget
 
 
 class ToolBarAction(ABC):
-    """Abstract base class for action creators for the toolbar."""
-
     @abstractmethod
-    def create(self, target: QWidget):
-        """Creates and returns an action to be added to a toolbar.
-
-        This method must be implemented by subclasses.
+    def add_to_toolbar(self, toolbar: QToolBar, target: QWidget):
+        """Adds an action or widget to a toolbar.
 
         Args:
-            target (QWidget): The widget that the action will target.
-
-        Returns:
-            QAction: The action created for the toolbar.
+            toolbar (QToolBar): The toolbar to add the action or widget to.
+            target (QWidget): The target widget for the action.
         """
 
 
-class OpenFileAction:  # (ToolBarAction):
-    """Action creator for the 'Open File' action in the toolbar."""
+class ColumnAdjustAction(ToolBarAction):
+    """Toolbar spinbox to adjust number of columns in the plot layout"""
 
-    def create(self, target: QWidget):
-        """Creates an 'Open File' action for the toolbar.
-
-        Args:
-            target (QWidget): The widget that the 'Open File' action will be targeted.
-
-        Returns:
-            QAction: The 'Open File' action created for the toolbar.
-        """
-        icon = QApplication.style().standardIcon(QStyle.StandardPixmap.SP_DialogOpenButton)
-        action = QAction(icon, "Open File", target)
-        # action = QAction("Open File", target)
-        action.triggered.connect(target.open_file)
-        return action
-
-
-class SaveFileAction:
-    """Action creator for the 'Save File' action in the toolbar."""
-
-    def create(self, target):
-        """Creates a 'Save File' action for the toolbar.
+    def add_to_toolbar(self, toolbar: QToolBar, target: QWidget):
+        """Creates a access history button for the toolbar.
 
         Args:
-            target (QWidget): The widget that the 'Save File' action will be targeted.
+            toolbar (QToolBar): The toolbar to add the action to.
+            target (QWidget): The widget that the 'Access Scan History' action will be targeted.
 
         Returns:
-            QAction: The 'Save File' action created for the toolbar.
+            QAction: The 'Access Scan History' action created for the toolbar.
         """
-        icon = QApplication.style().standardIcon(QStyle.StandardPixmap.SP_DialogSaveButton)
-        action = QAction(icon, "Save File", target)
-        # action = QAction("Save File", target)
-        action.triggered.connect(target.save_file)
-        return action
+        widget = QWidget()
+        layout = QHBoxLayout(widget)
 
+        label = QLabel("Columns:")
+        spin_box = QSpinBox()
+        spin_box.setMinimum(1)  # Set minimum value
+        spin_box.setMaximum(10)  # Set maximum value
+        spin_box.setValue(target.get_column_count())  # Initial value
+        spin_box.valueChanged.connect(lambda value: target.set_column_count(value))
 
-class RunScriptAction:
-    """Action creator for the 'Run Script' action in the toolbar."""
-
-    def create(self, target):
-        """Creates a 'Run Script' action for the toolbar.
-
-        Args:
-            target (QWidget): The widget that the 'Run Script' action will be targeted.
-
-        Returns:
-            QAction: The 'Run Script' action created for the toolbar.
-        """
-        icon = QApplication.style().standardIcon(QStyle.StandardPixmap.SP_MediaPlay)
-        action = QAction(icon, "Run Script", target)
-        # action = QAction("Run Script", target)
-        action.triggered.connect(target.run_script)
-        return action
+        layout.addWidget(label)
+        layout.addWidget(spin_box)
+        toolbar.addWidget(widget)
 
 
 class ModularToolBar(QToolBar):
     """Modular toolbar with optional automatic initialization.
-
     Args:
         parent (QWidget, optional): The parent widget of the toolbar. Defaults to None.
-        auto_init (bool, optional): If True, automatically populates the toolbar based on the parent widget.
+        actions (list[ToolBarAction], optional): A list of action creators to populate the toolbar. Defaults to None.
+        target_widget (QWidget, optional): The widget that the actions will target. Defaults to None.
+        color (str, optional): The background color of the toolbar. Defaults to "black".
     """
 
-    def __init__(self, parent=None, auto_init=True):
+    def __init__(self, parent=None, actions=None, target_widget=None, color: str = "black"):
         super().__init__(parent)
-        self.auto_init = auto_init
-        self.handler = {
-            "BECEditor": [OpenFileAction(), SaveFileAction(), RunScriptAction()],
-            # BECMonitor: [SomeOtherAction(), AnotherAction()],  # Example for another widget
-        }
-        self.setStyleSheet("QToolBar { background: transparent; }")
-        # Set the icon size for the toolbar
-        self.setIconSize(QSize(20, 20))
 
-        if self.auto_init:
-            QTimer.singleShot(0, self.auto_detect_and_populate)
+        self.widgets = defaultdict(dict)
+        self.set_background_color(color)
 
-    def auto_detect_and_populate(self):
-        """Automatically detects the parent widget and populates the toolbar with relevant actions."""
-        if not self.auto_init:
-            return
+        if actions is not None and target_widget is not None:
+            self.populate_toolbar(actions, target_widget)
 
-        parent_widget = self.parent()
-        if parent_widget is None:
-            return
-
-        parent_widget_class_name = type(parent_widget).__name__
-        for widget_type_name, actions in self.handler.items():
-            if parent_widget_class_name == widget_type_name:
-                self.populate_toolbar(actions, parent_widget)
-                return
-
-    def populate_toolbar(self, actions, target_widget):
+    def populate_toolbar(self, actions: dict, target_widget):
         """Populates the toolbar with a set of actions.
 
         Args:
@@ -125,20 +72,13 @@ class ModularToolBar(QToolBar):
             target_widget (QWidget): The widget that the actions will target.
         """
         self.clear()
-        for action_creator in actions:
-            action = action_creator.create(target_widget)
-            self.addAction(action)
+        for action_id, action in actions.items():
+            action.add_to_toolbar(self, target_widget)
+            self.widgets[action_id] = action
 
-    def set_manual_actions(self, actions, target_widget):
-        """Manually sets the actions for the toolbar.
-
-        Args:
-            actions (list[QAction or ToolBarAction]): A list of actions or action creators to populate the toolbar.
-            target_widget (QWidget): The widget that the actions will target.
-        """
-        self.clear()
-        for action in actions:
-            if isinstance(action, QAction):
-                self.addAction(action)
-            elif isinstance(action, ToolBarAction):
-                self.addAction(action.create(target_widget))
+    def set_background_color(self, color: str):
+        self.setStyleSheet(f"QToolBar {{ background: {color}; }}")
+        self.setIconSize(QSize(20, 20))
+        self.setMovable(False)
+        self.setFloatable(False)
+        self.setContentsMargins(0, 0, 0, 0)
