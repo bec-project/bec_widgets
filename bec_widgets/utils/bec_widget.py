08a39bf41c2f5bcaa39d89ba0bae0717d9b017fc
wakonig_k <klaus.wakonig@psi.ch>
2024-07-08 16:17:40 +0200

    fix(bec_widget): added cleanup method to bec widget base class

--- bec_widgets/utils/bec_widget.py
+++ bec_widgets/utils/bec_widget.py
@@ -1,2 +1,7 @@
 class BECWidget:
     """Base class for all BEC widgets."""
+
+    def closeEvent(self, event):
+        if hasattr(self, "cleanup"):
+            self.cleanup()
+        event.accept()
