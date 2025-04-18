9b6fff83766b731c0742ae7433b740aee3c0bd71
wakonig_k <klaus.wakonig@psi.ch>
2025-04-18 13:18:33 +0200

    wip

--- bec_widgets/widgets/plots/waveform/waveform.py
+++ bec_widgets/widgets/plots/waveform/waveform.py
@@ -1219,6 +1219,7 @@ class Waveform(PlotBase):
         else:
             stack_trace = traceback.extract_stack()
             logger.warning(f"Async readback without scan ID, stack trace: {stack_trace}")
+            logger.warning(f"Sender: {sender}, cb_info: {getattr(sender, 'cb_info', None)}")
         instruction = metadata.get("async_update", {}).get("type")
         if instruction not in ["add", "add_slice", "replace"]:
             logger.warning(f"Invalid async update instruction: {instruction}")
