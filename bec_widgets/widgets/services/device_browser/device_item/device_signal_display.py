7c9e1333bc7d26f6469019512e9d7e22c1c5a83f
David Perl <david.perl@psi.ch>
2025-07-17 18:52:40 +0200

    fix(device_browser): display signal for signals

--- bec_widgets/widgets/services/device_browser/device_item/device_signal_display.py
+++ bec_widgets/widgets/services/device_browser/device_item/device_signal_display.py
@@ -1,3 +1,4 @@
+from bec_lib.device import Device
 from bec_qthemes import material_icon
 from qtpy.QtCore import Qt
 from qtpy.QtWidgets import QHBoxLayout, QLabel, QToolButton, QVBoxLayout, QWidget
@@ -64,16 +65,30 @@ class SignalDisplay(BECWidget, QWidget):
         self._add_refresh_button()
 
         if self._device in self.dev:
-            for sig, info in self.dev[self.device]._info.get("signals", {}).items():
-                if info.get("kind_str") in [Kind.hinted.name, Kind.normal.name, Kind.config.name]:
-                    self._content_layout.addWidget(
-                        SignalLabel(
-                            device=self._device,
-                            signal=sig,
-                            show_select_button=False,
-                            show_default_units=True,
+            if isinstance(self.dev[self.device], Device):
+                for sig, info in self.dev[self.device]._info.get("signals", {}).items():
+                    if info.get("kind_str") in [
+                        Kind.hinted.name,
+                        Kind.normal.name,
+                        Kind.config.name,
+                    ]:
+                        self._content_layout.addWidget(
+                            SignalLabel(
+                                device=self._device,
+                                signal=sig,
+                                show_select_button=False,
+                                show_default_units=True,
+                            )
                         )
+            else:
+                self._content_layout.addWidget(
+                    SignalLabel(
+                        device=self._device,
+                        signal=self._device,
+                        show_select_button=False,
+                        show_default_units=True,
                     )
+                )
             self._content_layout.addStretch(1)
         else:
             self._content_layout.addWidget(
