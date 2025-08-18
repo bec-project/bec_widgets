7eb2ce5c1d2ff64ec4e80d3dce77915e730746f8
David Perl <david.perl@psi.ch>
2025-08-18 14:55:18 +0200

    feat: available resources basic structure

---
+++ bec_widgets/widgets/control/device_manager/components/available_device_resources/available_device_resources.py
@@ -0,0 +1,61 @@
+from random import randint
+from typing import Iterable
+
+from qtpy.QtCore import QSize
+from qtpy.QtWidgets import QListWidgetItem, QWidget
+
+from bec_widgets.utils.bec_widget import BECWidget
+from bec_widgets.widgets.control.device_manager.components.available_device_resources.available_device_resources_ui import (
+    Ui_availableDeviceResources,
+)
+from bec_widgets.widgets.control.device_manager.components.available_device_resources.device_resource_backend import (
+    HashableDevice,
+    get_backend,
+)
+from bec_widgets.widgets.control.device_manager.components.available_device_resources.device_tag_group import (
+    DeviceTagGroup,
+)
+
+
+class AvailableDeviceResources(BECWidget, QWidget, Ui_availableDeviceResources):
+    def __init__(self, parent=None, **kwargs):
+        super().__init__(parent=parent, **kwargs)
+        self.setupUi(self)
+        self._backend = get_backend()
+        self._items: dict[str, tuple[QListWidgetItem, DeviceTagGroup]] = {}
+        self.refresh_full_list()
+
+    def refresh_full_list(self):
+        self.tag_groups_list.clear()
+        self._items = {}
+        for tag_group, devices in self._backend.tag_groups.items():
+            item = QListWidgetItem(self.tag_groups_list)
+            tag_group_widget = DeviceTagGroup(self.tag_groups_list, tag_group, devices)
+            self.tag_groups_list.setItemWidget(item, tag_group_widget)
+            self.tag_groups_list.addItem(item)
+            self._items[tag_group] = (item, tag_group_widget)
+            item.setSizeHint(QSize(tag_group_widget.width(), tag_group_widget.height()))
+
+    def set_devices_state(self, devices: Iterable[HashableDevice], included: bool):
+        for _, tag_group in self._items.values():
+            for device in devices:
+                tag_group.set_item_state(hash(device), included)
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        for list_item, tag_group_widget in self._items.values():
+            list_item.setSizeHint(tag_group_widget.sizeHint())
+
+
+if __name__ == "__main__":
+    import sys
+
+    from qtpy.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    widget = AvailableDeviceResources()
+    widget.set_devices_state(
+        list(filter(lambda _: randint(0, 1) == 1, widget._backend.all_devices)), True
+    )
+    widget.show()
+    sys.exit(app.exec())
---
+++ bec_widgets/widgets/control/device_manager/components/available_device_resources/device_tag_group.py
@@ -0,0 +1,163 @@
+from textwrap import dedent
+from typing import Callable, NamedTuple
+
+from bec_qthemes import material_icon
+from qtpy.QtCore import QSize
+from qtpy.QtWidgets import QFrame, QHBoxLayout, QLabel, QListWidgetItem, QVBoxLayout, QWidget
+
+from bec_widgets.widgets.control.device_manager.components.available_device_resources.device_resource_backend import (
+    HashableDevice,
+)
+from bec_widgets.widgets.control.device_manager.components.available_device_resources.device_tag_group_item_ui import (
+    Ui_DeviceTagGroup,
+)
+
+DEVICE_HASH_ROLE = 101
+
+
+class _DeviceEntryWidget(QFrame):
+    _grid_size = QSize(120, 80)
+
+    def __init__(self, device_spec: HashableDevice, parent=None, **kwargs):
+        super().__init__(parent, **kwargs)
+        self._device_spec = device_spec
+        self.included: bool = False
+
+        self.setFrameShape(QFrame.Shape.StyledPanel)
+        self.setFrameShadow(QFrame.Shadow.Raised)
+
+        self._layout = QVBoxLayout()
+        self._layout.setContentsMargins(5, 5, 5, 5)
+        self.setLayout(self._layout)
+        self.setMinimumSize(self._grid_size)
+
+        self.setup_title_layout(device_spec)
+
+        self.setToolTip(device_spec.rich_text())
+
+        self.details = QLabel(f"Tags:\n{', '.join(device_spec.deviceTags)}")
+        self.details.setStyleSheet("QLabel { font-size: 8pt; }")
+        self.details.setWordWrap(True)
+        self._layout.addWidget(self.details)
+
+    def setup_title_layout(self, device_spec: HashableDevice):
+        self._title_layout = QHBoxLayout()
+        self._title_container = QWidget(parent=self)
+        self._title_container.setLayout(self._title_layout)
+
+        self.title = QLabel(device_spec.name)
+        self.title.setToolTip(device_spec.name)
+        self.title.setStyleSheet(self.title_style("#FF0000"))
+        self._title_layout.addWidget(self.title)
+
+        self._title_layout.addStretch(1)
+
+        self._warning_label = QLabel()
+        self._title_layout.addWidget(self._warning_label)
+
+        self._layout.addWidget(self._title_container)
+
+    def check_and_display_warning(self):
+        if len(self._device_spec.names) == 1 and len(self._device_spec.source_files) == 1:
+            self._warning_label.setText("")
+            self._warning_label.setToolTip("")
+        else:
+            self._warning_label.setPixmap(material_icon("warning", color="#FFAA00"))
+            self._warning_label.setToolTip(
+                dedent(
+                    f"""
+                    {f"Device has multiple names! Please check! \n names: {self._device_spec.names}" if len(self._device_spec.names)>1 else ""}
+                    {f"Device found in multiple source files! Please check! \n files: {self._device_spec.names}" if len(self._device_spec.names)>1 else ""}
+                    """
+                )
+            )
+
+    @property
+    def device_hash(self):
+        return hash(self._device_spec)
+
+    def title_style(self, color: str) -> str:
+        return f"QLabel {{ color: {color}; font-weight: bold; font-size: 10pt; }}"
+
+    def setTitle(self, text: str):
+        self.title.setText(text)
+
+    def set_included(self, included: bool):
+        self.included = included
+        self.title.setStyleSheet(self.title_style("#00FF00" if included else "#FF0000"))
+
+
+class _DeviceEntry(NamedTuple):
+    list_item: QListWidgetItem
+    widget: _DeviceEntryWidget
+
+
+class DeviceTagGroup(QWidget, Ui_DeviceTagGroup):
+    def __init__(
+        self, parent=None, name: str = "TagGroupTitle", data: set[HashableDevice] = set(), **kwargs
+    ):
+        super().__init__(parent=parent, **kwargs)
+        self.setupUi(self)
+        self.device_list.setGridSize(_DeviceEntryWidget._grid_size)
+        self.title.setText(name)
+        self._devices: dict[str, _DeviceEntry] = {}
+        for device in data:
+            self._add_item(device)
+        self.device_list.sortItems()
+
+    def _add_item(self, device: HashableDevice):
+        item = QListWidgetItem(self.device_list)
+        widget = _DeviceEntryWidget(device, self)
+        item.setSizeHint(QSize(widget.width(), widget.height()))
+        self.device_list.setItemWidget(item, widget)
+        self.device_list.addItem(item)
+        self._devices[device.name] = _DeviceEntry(item, widget)
+
+    def set_item_state(self, /, device_hash: int, included: bool):
+        for dev in self._devices.values():
+            if dev.widget.device_hash == device_hash:
+                dev.widget.set_included(included)
+        self._update_num_included()
+
+    def _update_num_included(self):
+        n_included = sum(int(dev.widget.included) for dev in self._devices.values())
+        if n_included == 0:
+            color = "#FF0000"
+        elif n_included == len(self._devices):
+            color = "#00FF00"
+        else:
+            color = "#FFAA00"
+        self.n_included.setText(f"{n_included} / {len(self._devices)}")
+        self.n_included.setStyleSheet(f"QLabel {{ color: {color}; }}")
+
+    def resizeEvent(self, event):
+        super().resizeEvent(event)
+        self.setMinimumHeight(self.sizeHint().height())
+        self.setMaximumHeight(self.sizeHint().height())
+
+    def __repr__(self) -> str:
+        return f"{self.__class__.__name__}: {self.title.text()}"
+
+
+if __name__ == "__main__":
+    import sys
+
+    from qtpy.QtWidgets import QApplication
+
+    app = QApplication(sys.argv)
+    widget = DeviceTagGroup(name="Tag group 1")
+    for item in [
+        HashableDevice(
+            **{
+                "name": f"test_device_{i}",
+                "deviceClass": "TestDeviceClass",
+                "readoutPriority": "baseline",
+                "enabled": True,
+            }
+        )
+        for i in range(5)
+    ]:
+        widget._add_item(item)
+    widget._update_num_included()
+    widget.show()
+    sys.exit(app.exec())
