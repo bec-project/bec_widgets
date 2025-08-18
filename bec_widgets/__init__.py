d4ea9f29924817e4f024dbfa8ff2cbb695812c87
wyzula-jan <jan.wyzula@psi.ch>
2025-08-18 23:06:11 +0200

    fix(advanced_dock_area): dock manager global flags initialised in BW init to prevent segfault

--- bec_widgets/__init__.py
+++ bec_widgets/__init__.py
@@ -1,6 +1,8 @@
 import os
 import sys
 
+import PySide6QtAds as QtAds
+
 from bec_widgets.utils.bec_widget import BECWidget
 from bec_widgets.utils.error_popups import SafeProperty, SafeSlot
 
@@ -8,5 +10,11 @@ if sys.platform.startswith("linux"):
     qt_platform = os.environ.get("QT_QPA_PLATFORM", "")
     if qt_platform != "offscreen":
         os.environ["QT_QPA_PLATFORM"] = "xcb"
+
+# Default QtAds configuration
+QtAds.CDockManager.setConfigFlag(QtAds.CDockManager.eConfigFlag.FocusHighlighting, True)
+QtAds.CDockManager.setConfigFlag(
+    QtAds.CDockManager.eConfigFlag.RetainTabSizeWhenCloseButtonHidden, True
+)
 
 __all__ = ["BECWidget", "SafeSlot", "SafeProperty"]
