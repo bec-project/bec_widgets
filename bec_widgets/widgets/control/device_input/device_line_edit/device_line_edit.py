18ffe0584fd1bcefaa4400298d5459b9d0f38dcc
wyzula-jan <jan.wyzula@psi.ch>
2025-08-19 10:50:56 +0200

    fix(bec_widgets): adapt to bec_qthemes 1.0

--- bec_widgets/widgets/control/device_input/device_line_edit/device_line_edit.py
+++ bec_widgets/widgets/control/device_input/device_line_edit/device_line_edit.py
@@ -175,13 +175,13 @@ if __name__ == "__main__":  # pragma: no cover
     # pylint: disable=import-outside-toplevel
     from qtpy.QtWidgets import QVBoxLayout, QWidget
 
-    from bec_widgets.utils.colors import set_theme
+    from bec_widgets.utils.colors import apply_theme
     from bec_widgets.widgets.control.device_input.signal_combobox.signal_combobox import (
         SignalComboBox,
     )
 
     app = QApplication([])
-    set_theme("dark")
+    apply_theme("dark")
     widget = QWidget()
     widget.setFixedSize(200, 200)
     layout = QVBoxLayout()
--- bec_widgets/widgets/services/device_browser/device_item/device_signal_display.py
+++ bec_widgets/widgets/services/device_browser/device_item/device_signal_display.py
@@ -110,10 +110,10 @@ if __name__ == "__main__":  # pragma: no cover
 
     from qtpy.QtWidgets import QApplication
 
-    from bec_widgets.utils.colors import set_theme
+    from bec_widgets.utils.colors import apply_theme
 
     app = QApplication(sys.argv)
-    set_theme("light")
+    apply_theme("light")
     widget = SignalDisplay(device="samx")
     widget.show()
     sys.exit(app.exec_())
