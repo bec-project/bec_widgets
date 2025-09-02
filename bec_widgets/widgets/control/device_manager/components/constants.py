6af2b09f19c1fa3ef5bc47526cc35dcee8ce65ce
David Perl <david.perl@psi.ch>
2025-09-02 15:05:06 +0200

    fix(dm): add constants.py

---
+++ bec_widgets/widgets/control/device_manager/components/constants.py
@@ -0,0 +1,5 @@
+from typing import Final
+
+# Denotes a MIME type for JSON-encoded list of device config dictionaries
+MIME_DEVICE_CONFIG: Final[str] = "application/x-bec_device_config"
+CONFIG_DATA_ROLE: Final[int] = 118
