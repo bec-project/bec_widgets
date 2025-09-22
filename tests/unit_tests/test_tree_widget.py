227d6b3e451f2dbe3627e727c90d79f129d94c52
wyzula-jan <jan.wyzula@psi.ch>
2025-09-22 08:39:29 -0500

    wip dummy test

---
+++ tests/unit_tests/test_tree_widget.py
@@ -0,0 +1,23 @@
+import pytest
+from PySide6.QtWidgets import QTreeView, QWidget
+
+from bec_widgets.utils.colors import apply_theme
+
+
+class DummyTree(QWidget):
+    def __init__(self):
+        super().__init__()
+        tree = QTreeView(self)
+        apply_theme("dark")
+
+
+@pytest.fixture
+def tree_widget(qtbot):
+    tree = DummyTree()
+    qtbot.addWidget(tree)
+    qtbot.waitExposed(tree)
+    yield tree
+
+
+def test_tree_widget_init(tree_widget):
+    assert isinstance(tree_widget, QWidget)
