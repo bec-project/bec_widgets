9d92f8b53a6ffe57a9dffad797580228023bf6e1
wakonig_k <klaus.wakonig@psi.ch>
2026-05-05 16:01:06 +0200

    fix(web_links): update documentation links in BECWebLinksMixin

--- bec_widgets/widgets/containers/main_window/addons/web_links.py
+++ bec_widgets/widgets/containers/main_window/addons/web_links.py
@@ -4,11 +4,7 @@ import webbrowser
 class BECWebLinksMixin:
     @staticmethod
     def open_bec_docs():
-        webbrowser.open("https://beamline-experiment-control.readthedocs.io/en/latest/")
-
-    @staticmethod
-    def open_bec_widgets_docs():
-        webbrowser.open("https://bec.readthedocs.io/projects/bec-widgets/en/latest/")
+        webbrowser.open("https://bec-project.github.io/bec_docs/")
 
     @staticmethod
     def open_bec_bug_report():
